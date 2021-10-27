488edbb4d1486d2460dded2ada1795acd41aa563
Szabolcs Csizmadia <csizmadia98@gmail.com>
2021-10-27 23:13:59 +0200

    feat: upgrade esbuild version 0.13.X, upgrade vm2 version to 3.9.5, remove website #16

--- apps/espack-website/shims/react-shim.mjs
+++
@@ -1,1 +0,0 @@
-export { createElement, Fragment } from 'react';
--- apps/espack-website/src/app/app.tsx
+++
@@ -1,9 +0,0 @@
-import { Test } from './components/test/test';
-
-type AppComponent = () => JSX.Element;
-
-export const App: AppComponent = () => (
-    <div>
-        <Test />
-    </div>
-);
